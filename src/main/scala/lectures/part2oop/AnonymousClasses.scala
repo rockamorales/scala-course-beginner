eefbb93e476c2e25db683e38ea03233018ddfe86
Roberto Morales <rcmorales@applaudostudios.com>
2022-08-28 20:06:50 -0600

    Anonymous classes

---
+++ src/main/scala/lectures/part2oop/AnonymousClasses.scala
@@ -0,0 +1,22 @@
+package lectures.part2oop
+
+object AnonymousClasses extends App{
+  abstract class Animal {
+    def eat: Unit
+  }
+
+  val funnyAnimal = new Animal {
+    override def eat: Unit = println("hahahahaha")
+  }
+
+  println(funnyAnimal.getClass)
+
+  class Person(name: String) {
+    def sayHi: Unit = println(s"Hi, my name is $name, how can I help?")
+  }
+
+  val jim = new Person("Jim"){
+    override def sayHi: Unit = println(s"Hi, my name is Jim, how can I be of service?")
+  }
+
+}
