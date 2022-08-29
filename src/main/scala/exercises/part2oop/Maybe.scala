f795bd1ed757ae0143623352276ce9dbec44b826
Roberto Morales <rcmorales@applaudostudios.com>
2022-08-29 17:56:58 -0600

    Refactoring maybe implementations

--- src/main/scala/exercises/part2oop/Maybe.scala
+++ src/main/scala/exercises/part2oop/Maybe.scala
@@ -1,5 +1,7 @@
 package exercises.part2oop
 
+import java.util.NoSuchElementException
+
 abstract class Maybe[+A] {
   def get(): A
   def map[B](f: A => B): Maybe[B]
@@ -7,28 +9,36 @@ abstract class Maybe[+A] {
   def filter(f: A => Boolean): Maybe[A]
 }
 
-object Empty extends Maybe[Nothing] {
-  override def get(): Nothing = throw NoSuchElementException
+ case object MaybeNot extends Maybe[Nothing] {
+  override def get(): Nothing = throw new NoSuchElementException
 
-  override def map[B](f: Nothing => B): Maybe[Nothing] = Empty
+  override def map[B](f: Nothing => B): Maybe[Nothing] = MaybeNot
 
-  override def flatMap[B](f: Nothing => Maybe[B]): Maybe[Nothing] = Empty
+  override def flatMap[B](f: Nothing => Maybe[B]): Maybe[Nothing] = MaybeNot
 
-  override def filter(f: Nothing => Boolean): Maybe[Nothing] = Empty
+  override def filter(f: Nothing => Boolean): Maybe[Nothing] = MaybeNot
 }
 
-case class Some[+A](elem: A) extends Maybe[A] {
+case class Just[+A](elem: A) extends Maybe[A] {
   override def get(): A = elem
 
   override def map[B](f: A => B): Maybe[B] =
-    Some(f(elem))
+    Just(f(elem))
 
   override def flatMap[B](f: A => Maybe[B]): Maybe[B] =
     f(elem)
 
   override def filter(f: A => Boolean): Maybe[A] =
     if (f(elem)) this
-    else Empty
+    else MaybeNot
+}
+
+object MaybeTest extends App {
+  val just3 = Just(3)
+  println(just3)
+  println(just3.map(_ * 2))
+  println(just3.flatMap(x => Just(x % 2 == 0)))
+  println(just3.filter(_ % 2 == 0))
 }
 
 
